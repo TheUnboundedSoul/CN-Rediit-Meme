01b7a41adf1b76690e4a9dbe3e4b297150ed6c92
Guilherme <oliveira.guiribeiro.guilherme@gmail.com>
2023-06-07 08:53:41 +0100

    "Adicição de documentação no ficheiro db_memes.py"

--- meme_flask/BD/db_memes.py
+++ meme_flask/BD/db_memes.py
@@ -15,6 +15,61 @@ theConnection = mysql.connector.connect(
 DB_NAME = "meme_db"
 TABLE_URLS_NAME = "tUrls"
 
+INSERT_RECORD_STATEMENT = "insert into `%s`.`%s` values(\
+    null,\
+    '%s',\
+    '%s',\
+    '%s',\
+    '%s'\
+);"
+
+from datetime import date, datetime
+def utilNowYMDHMS():
+    dateToday = date.today()
+    y, m, d = dateToday.year, dateToday.month, dateToday.day
+    sy = str(y)
+    sm = ""
+    sd = ""
+
+    if (m < 10):
+        sm = "0" + str(m)
+    else:
+        sm = str(m)
+
+    if (d < 10):
+        sd = "0" + str(d)
+    else:
+        sd = str(d)
+
+    strYMD = "%s-%s-%s" % (sy, sm, sd)
+
+    timeNow = datetime.now()
+    hh, mm, ss = timeNow.hour, timeNow.minute, timeNow.second
+    shh = ""
+    smm = ""
+    sss = ""
+    if (hh < 10):
+        shh = "0" + str(hh)
+    else:
+        shh = str(hh)
+
+    if (mm < 10):
+        smm = "0" + str(mm)
+    else:
+        smm = str(mm)
+
+    if (ss < 10):
+        sss = "0" + str(ss)
+    else:
+        sss = str(ss)
+
+    strHMS = "%s:%s:%s" % (shh, smm, sss)
+
+    strRet = "%s %s" % (strYMD, strHMS)
+
+    return strRet
+# def utilNowYMDHMS
+
 def getInsertUrlSt(
     pStrMemeUrl:str,
     pStrRedditName:str,
